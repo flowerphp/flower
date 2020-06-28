90a66bc8a81992663c7f03463306c4de73a7a833
FlexICE <one.fl3xice@yandex.ru>
2020-06-28 20:44:12 +0300

    As Extreme Promises must have final i make his final, iforgot make this yesterday

--- app/core/promises/ExtremeMysqlDatabasesConfigPromise.php
+++ app/core/promises/ExtremeMysqlDatabasesConfigPromise.php
@@ -4,7 +4,7 @@
 namespace App\Core\Promises;
 
 
-class ExtremeMysqlDatabasesConfigPromise extends ExtremePromise
+final class ExtremeMysqlDatabasesConfigPromise extends ExtremePromise
 {
     private $enabled;
     private $host;
